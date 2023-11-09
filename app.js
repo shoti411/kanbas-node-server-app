32ddacf3dd0881c9e6199084493a1d3328905363
lcpo <po.luk@northeastern.edu>
2023-11-09 13:10:26 -0500

    continuing exercises for a5

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 // Entry point into node.js http server
 
 import express from "express";
+import Hello from "./hello.js";
 // const express = require("express");
-import Lab5 from "./lab5";
-
+import Lab5 from "./lab5.js";
 
 const app = express();
 
-
-
-
-
-const hello = (req, res) => {
-    res.send("Life is Good!");
-};
-
-app.get("/hello", hello);
-
-app.get("/", (req, res) => {
-    res.send("Welcome to Assignment 5!");
-});
-
+Hello(app);
 Lab5(app);
+
+
+app.use(express.json());
+
+
+// const hello = (req, res) => {
+//     res.send("Life is Good!");
+// };
+
+// app.get("/hello", hello);
+
+// app.get("/a5", Lab5(app));
+
+// app.get("/", (req, res) => {
+//     res.send("Welcome to Assignment 5!");
+    
+// });
+
 
 app.listen(4000);
 
