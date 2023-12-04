729059babe2526fe1ca49f791e248135aa790901
lcpo <po.luk@northeastern.edu>
2023-12-04 14:02:04 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -16,24 +16,24 @@ mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
+  credentials: true,
+  origin: process.env.FRONTEND_URL,
 }));
 const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
   };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-    };
-  }
-  app.use(session(sessionOptions));
-  
-  
+}
+app.use(session(sessionOptions));
+
+
 app.use(express.json());
 
 UserRoutes(app);
@@ -55,7 +55,7 @@ Lab5(app);
 
 // app.get("/", (req, res) => {
 //     res.send("Welcome to Assignment 5!");
-    
+
 // });
 
 const port = process.env.PORT || 4000;
